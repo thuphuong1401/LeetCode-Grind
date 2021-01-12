88934d830f9afb963656d8ee87e0b252207b5f8e
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2021-01-12 18:06:46 +0700

    Update 73.Set-Matrix-Zeroes.java

--- Array/73.Set-Matrix-Zeroes.java
+++ Array/73.Set-Matrix-Zeroes.java
@@ -37,3 +37,65 @@ class Solution {
         }
     }
 }
+
+
+
+
+// O(1) space, in theory
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        boolean firstRowIsZero = false;
+        boolean firstColIsZero = false;
+        
+        // first row
+        for(int i = 0; i < n; i++) {
+            if(matrix[0][i] == 0) {
+                firstRowIsZero = true;
+                break;
+            }
+        }
+        
+        // first col
+        for(int i = 0; i < m; i++) {
+            if(matrix[i][0] == 0) {
+                firstColIsZero = true;
+                break;
+            }
+        }
+        
+        // set 0's for the rest and record them on 1st row and 1st col
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        // set 0's
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        // process the 1st row and 1st col
+        if(firstRowIsZero) {
+            for(int i = 0; i < n; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        
+        if(firstColIsZero) {
+            for(int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
