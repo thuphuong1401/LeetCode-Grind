42aeb1cb4c8cc4d62e7e94b89616540abbc0b6b8
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2021-01-15 15:09:07 +0700

    Update 257.Binary-Tree-Paths.java

--- Tree/257.Binary-Tree-Paths.java
+++ Tree/257.Binary-Tree-Paths.java
@@ -45,5 +45,49 @@ class Solution {
         }
         sb.setLength(length); // backtrack
     }
+}
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// Recursive but not using StringBuilder
+class Solution {
+    List<String> answer;
     
+    public List<String> binaryTreePaths(TreeNode root) {
+        answer = new ArrayList<>();
+        traverse(root, "");
+        return answer;
+    }
+    
+    public void traverse(TreeNode root, String s) {
+        if(root == null) {
+            return;
+        }
+        
+        s += root.val;
+        
+        if(root.left == null && root.right == null) {
+            answer.add(s);
+        } else {
+            s += "->";
+            traverse(root.left, s);
+            traverse(root.right, s);
+        }
+    }   
 }
