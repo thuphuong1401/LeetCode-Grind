bd7da7a19d18bf3da66af1f648c6035f81ed8c1f
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2021-01-16 14:57:05 +0700

    Create 450.Delete-Node-In-A-BST.java

---
+++ Tree/450.Delete-Node-In-A-BST.java
@@ -0,0 +1,59 @@
+/*
+https://leetcode.com/problems/delete-node-in-a-bst/
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        return delete(root, key);
+    }
+    
+    private TreeNode delete(TreeNode root, int key) {
+        if(root == null) {
+            return root;
+        }
+        
+        // locate the node to remove
+        if(root.val > key) {
+            root.left = delete(root.left, key);
+        } else if(root.val < key) {
+            root.right = delete(root.right, key);
+        } else { // found it - 3 cases
+            if(root.left == null) { // no left subtree => replace node to delete by root of right subtree
+                return root.right;
+            } else if(root.right == null) { // no right subtree => replace node to delete by root of left subtree
+                return root.left;
+            } else { // both left & right subtree exists
+                TreeNode temp = minVal(root.right);
+                root.val = temp.val;
+                root.right = delete(root.right, temp.val);
+            }
+        }
+        return root;
+    }
+        
+    
+    
+    private TreeNode minVal(TreeNode root) {
+        TreeNode temp = root;
+        while(temp.left != null) {
+            temp = temp.left;
+        }
+        return temp;
+    }
+    
+}
