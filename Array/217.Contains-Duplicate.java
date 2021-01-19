a6d47ec4ae2b8c27ddfcf579ed0369725c7cbc72
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2021-01-19 22:29:52 +0700

    Create 217.Contains-Duplicate.java

---
+++ Array/217.Contains-Duplicate.java
@@ -0,0 +1,17 @@
+/*
+https://leetcode.com/problems/contains-duplicate/
+*/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(!set.contains(nums[i])) {
+                set.add(nums[i]);
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
