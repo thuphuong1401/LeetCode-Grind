2f09eb79684a05aeeae7d1d54436c67ab30a6047
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2021-01-26 14:24:42 +0700

    Update 121.Best-Time-To-Buy-And-Sell-Stock.java

--- Dynamic Programming/121.Best-Time-To-Buy-And-Sell-Stock.java
+++ Dynamic Programming/121.Best-Time-To-Buy-And-Sell-Stock.java
@@ -2,6 +2,14 @@
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 */
 
+
+/*
+One pass - Time O(n), Space O(1)
+Idea: 
+- Loop through every element of the array
+- At every point, update the minPrice (lowest buy point so far) and maxProfit. 
+- Update maxProfit so far if the difference (currProfit - minPriceSoFar) is larger 
+*/
 class Solution {
     public int maxProfit(int[] prices) {
         int maxProfit = 0;
