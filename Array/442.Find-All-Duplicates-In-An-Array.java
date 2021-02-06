35fee0c948f34034763dd095065d42c150cfd625
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2021-02-06 18:03:07 +0700

    Update 442.Find-All-Duplicates-In-An-Array.java

--- Array/442.Find-All-Duplicates-In-An-Array.java
+++ Array/442.Find-All-Duplicates-In-An-Array.java
@@ -21,4 +21,24 @@ class Solution {
 
 
 // O(n) time, O(1) space, so smart!
-
+/*
+Idea: 
+- Since 1 <= a_i <= n, therefore each element value - 1 is a valid index
+- Loop through the array. Mark arr[i-1] to be negative. If arr[i-1] has already been set to negative, we know that this element is repeated.
+*/
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> answer = new ArrayList<>();
+        int n = nums.length;
+        for(int i = 0; i < n; i++) {
+            int curr = Math.abs(nums[i]);
+            if(nums[curr - 1] < 0) {
+                answer.add(Math.abs(nums[i]));
+            }   
+            nums[curr - 1] = -nums[curr-1];
+        }
+        
+        return answer;
+        
+    }
+}
