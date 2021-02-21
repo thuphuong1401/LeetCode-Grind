7d12bf122c0e35119676b338df33d1d6afda9db7
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2021-02-21 18:03:08 +0700

    Create 172.Factorial-Trailing-Zeros.java

---
+++ Math/172.Factorial-Trailing-Zeros.java
@@ -0,0 +1,15 @@
+/*
+https://leetcode.com/problems/factorial-trailing-zeroes/
+*/
+
+class Solution {
+    public int trailingZeroes(int n) {
+        int zeroCount = 0;
+        long currentMultiple = 5;
+        while(n > 0) {
+            n /= 5;
+            zeroCount += n;
+        }
+        return zeroCount;
+    }
+}
