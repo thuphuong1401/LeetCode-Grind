60f53bda169d5bfbbd9bc337a01a41936c4d0731
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2021-03-16 14:07:22 +0700

    Create 35.Search-Insert-Position.java

---
+++ Array/35.Search-Insert-Position.java
@@ -0,0 +1,33 @@
+class Solution {
+    
+    // binary search first position >= target
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int index = -1;
+        
+        if(target <= nums[0]) {
+            return 0;
+        } 
+        if(nums[high] == target) {
+            return high;
+        } 
+        if(target > nums[high]) {
+            return high+1;
+        }
+        
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            if(mid != 0 && nums[mid] >= target && nums[mid-1] < target) {
+                index = mid;
+                break;
+            } else if(nums[mid] < target) {
+                low = mid+1;
+            } else if(nums[mid] > target) {
+                high = mid-1;
+            }
+            System.out.println(low + " " + high);
+        }
+        return index;
+    }
+}
