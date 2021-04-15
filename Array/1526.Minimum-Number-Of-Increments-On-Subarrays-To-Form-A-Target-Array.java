135073b81bd770a7394f006b5c0ed33f33528b64
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2021-04-15 22:38:40 +0700

    Create 1526.Minimum-Number-Of-Increments-On-Subarrays-To-Form-A-Target-Array.java

---
+++ Array/1526.Minimum-Number-Of-Increments-On-Subarrays-To-Form-A-Target-Array.java
@@ -0,0 +1,14 @@
+/*
+https://leetcode.com/problems/minimum-number-of-increments-on-subarrays-to-form-a-target-array/
+Very smart problem. Learn the 'wall of bricks' approach.
+*/
+class Solution {
+    public int minNumberOperations(int[] target) {
+        int n = target.length;
+        int res = 0;
+        for(int i = 1; i < n; i++) {
+            res += Math.max((target[i] - target[i-1]), 0);
+        }
+        return res;
+    }
+}
