3eaf17e3adcfa329ca92baf19da6d5fadf3495b0
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2021-04-19 00:22:06 +0700

    Update 1335.Minimum-Difficulty-Of-A-Job-Schedule.py

--- Dynamic Programming/1335.Minimum-Difficulty-Of-A-Job-Schedule.py
+++ Dynamic Programming/1335.Minimum-Difficulty-Of-A-Job-Schedule.py
@@ -2,6 +2,37 @@
 https://leetcode.com/problems/minimum-difficulty-of-a-job-schedule/
 '''
 
+''' 
+  dp[i][j]: min diff chia j doan tu 0 toi i (n * d)
+  - dp[i - 1][j]
+  - dp[i][j - 1]: min diff chia j - 1 doan tu dau toi i
+  - dp[i - 1][j - 1]: 
+  +
+  dp[i][j] vs dp[i-1][j-1]?
+  
+  
+  dp[i][j] = min(dp[i][j], dp[i-1][j-1] + max?)
+}
+
+dp[i][j]: min diff chia j doan tu dau toi phan tu thu i 
+
+[6, 1, 2, 3, 4, 5]  d = 
+dp[0][1] = 6
+dp[1][1] = 6
+dp[1][2] = dp[1][1] + max(...)
+dp[2][1] = max(arr[2], arr[1])
+dp[2][2] = min(dp[2][2], 
+dp[0][1] + max(arr[1:]),
+dp[1][1] + max(arr[2:])
+)
+dp[3][1] = max(arr[3], arr[2], arr[1])
+dp[3][2] = min(dp[3][2],
+dp[0][2] + max(arr[1:]),
+dp[1][2] + max(arr[2:]),
+dp[2][2] + max(arr[3:]),
+)
+
+'''
 class Solution:
     
     def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
