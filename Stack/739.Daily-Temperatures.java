65f6a288e2b3b9b3e265377999981fb87c44e3a3
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2021-04-27 13:33:24 +0700

    Update 739.Daily-Temperatures.java

--- Stack/739.Daily-Temperatures.java
+++ Stack/739.Daily-Temperatures.java
@@ -21,3 +21,31 @@ class Solution {
 
 
 // Stack
+// Maintain a decreasing stack
+class Point {
+    int value;
+    int index;
+    
+    public Point(int value, int index) {
+        this.value = value;
+        this.index = index;
+    }
+}
+
+class Solution {
+    public int[] dailyTemperatures(int[] T) {
+        int n = T.length;
+        int[] ans = new int[n];
+        Stack<Point> stack = new Stack<>();        
+        for(int i = 0; i < n; i++) {
+            while(!stack.isEmpty() && stack.peek().value < T[i]) {
+                int dist = i - stack.peek().index;
+                ans[stack.peek().index] = dist;
+                stack.pop();
+            }   
+            stack.push(new Point(T[i], i));
+        }
+        return ans;
+        
+    }
+} 
