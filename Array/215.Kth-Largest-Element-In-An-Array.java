64e6e4ef7d5905253333abbd945409015d608c6d
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2021-07-20 18:49:08 +0700

    Update 215.Kth-Largest-Element-In-An-Array.java

--- Array/215.Kth-Largest-Element-In-An-Array.java
+++ Array/215.Kth-Largest-Element-In-An-Array.java
@@ -41,3 +41,57 @@ class Solution {
     }
     
 }
+
+
+/*
+Another version with random index
+*/
+import java.lang.*;
+
+class Solution {
+    
+    Random rand = new Random();
+    
+    public int findKthLargest(int[] nums, int k) {
+        int targetIndex = nums.length - k;
+        return quickSelect(nums, targetIndex, 0, nums.length - 1);
+    }
+    
+    private int quickSelect(int[] nums, int targetIndex, int left, int right) {
+        if(left <= right) {
+            int pivotIndex = partition(nums, left, right);
+            if(pivotIndex == targetIndex) {
+                return nums[pivotIndex];
+            } else if(pivotIndex > targetIndex) {
+                return quickSelect(nums, targetIndex, left, pivotIndex - 1);
+            } else {
+                return quickSelect(nums, targetIndex, pivotIndex + 1, right);
+            }
+        }
+        return -1;
+    }
+    
+    private int partition(int[] nums, int left, int right) {
+        int index = left - 1;
+        int l = right - left + 1;
+        
+        int randIndex = left + rand.nextInt(l);
+        
+        int pivot = nums[randIndex];
+        int temp = nums[right];
+        nums[right] = nums[randIndex];
+        nums[randIndex] = temp;
+        
+        for(int i = left; i <= right; i++) {
+            if(nums[i] <= pivot) {
+                index++;
+                temp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = temp;
+            }
+        }
+        
+        return index;
+    }
+    
+}
