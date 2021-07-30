caced3cb551e91b4b25802d727f1fce1fc6a6fcb
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2021-07-30 14:13:59 +0700

    Create 75.Sort-Colors.java

---
+++ Sort/75.Sort-Colors.java
@@ -0,0 +1,33 @@
+/*
+https://leetcode.com/problems/sort-colors/
+*/
+
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int l = 0;
+        int m = 0;
+        int h = n-1;
+        
+        while(m <= h) {
+            if(nums[m] == 0) {
+                swap(nums, m, l);
+                l++;
+                m++;
+            } else if(nums[m] == 1) {
+                m++;
+            } else {
+                swap(nums, m, h);
+                h--;   
+            }
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    } 
+    
+}
