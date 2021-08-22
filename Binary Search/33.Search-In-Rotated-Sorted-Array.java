10eae31ac3dfcd16af417a441ef23402be42627d
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2021-08-21 23:23:02 -0400

    Update 33.Search-In-Rotated-Sorted-Array.java

--- Binary Search/33.Search-In-Rotated-Sorted-Array.java
+++ Binary Search/33.Search-In-Rotated-Sorted-Array.java
@@ -4,26 +4,31 @@ https://leetcode.com/problems/search-in-rotated-sorted-array/
 
 class Solution {
     public int search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
-        while(low <= high) {
-            int mid = low + (high - low)/2;
+        int n = nums.length;
+        int left = 0;
+        int right = n-1;
+        while(left <= right) {
+            int mid = left + (right - left)/2;
             if(nums[mid] == target) {
                 return mid;
-            } else if(nums[mid] >= nums[low]) { // if arr[low...mid] is sorted
-                if(nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;
+            }
+            
+            if(nums[mid] < nums[left]) { // reflection point is in [left, mid]. CANNOT have <= here. '=' happens when array has 2 elements left to search. 
+                if(target >= nums[left] || target < nums[mid]) {
+                    right = mid - 1;
                 } else {
-                    low = mid + 1;
+                    left = mid + 1;
                 }
-            } else {
-                if(nums[mid] < target && target <= nums[high]) {
-                    low = mid + 1;
+            } else { // reflection point is in [mid, right] OR there's no reflection point at all
+                if(target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
                 } else {
-                    high = mid - 1;
+                    left = mid + 1;
                 }
             }
         }
         return -1;
     }
 }
+
+
