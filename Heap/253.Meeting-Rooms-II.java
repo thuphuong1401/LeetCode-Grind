97cf1b6c2ec11502059e87d3c60e01e641d85aec
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2021-08-26 22:04:38 -0400

    Update 253.Meeting-Rooms-II.java

--- Heap/253.Meeting-Rooms-II.java
+++ Heap/253.Meeting-Rooms-II.java
@@ -10,6 +10,9 @@ for interval in intervals:
   else:
     allocate new room
 return size of list of rooms
+
+Big idea: 
+- At every interval, choose any unoccupied room is OK => 2nd solution: instead of choosing randomly (by looping through array), we choose the room having the earliest end time.
 */
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
@@ -36,20 +39,46 @@ class Solution {
         return allRooms.size();
     }
     
-    
-    
     private boolean isAvailable(List<int[]> rooms, int[] currInterval) {
         int n = rooms.size();
         if(rooms.get(n-1)[1] <= currInterval[0]) {
             return true;
         }
-        for(int i = 0; i < n-1; i++) {
-            if(currInterval[0] >= rooms.get(i)[1] && currInterval[1] <= rooms.get(i+1)[0]) {
-                return true;
+        return false;
+    }    
+}
+
+
+
+
+/*
+2nd solution
+*/
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        int n = intervals.length;
+        PriorityQueue<Integer> pq = new PriorityQueue();
+        Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
+        int roomCount = 1;
+        pq.add(intervals[0][1]);
+        for(int i = 1; i < n; i++) {
+            int[] currInterval = intervals[i];
+            int start = currInterval[0];
+            int end = currInterval[1];
+            if(pq.peek() <= start) { // there is a room available for this interval
+                pq.remove();
+                pq.add(end);
+            } else {
+                pq.add(end);
+                roomCount++;
             }
         }
-        return false;
+        return roomCount;
     }
 }
+
+
+
+
 
 
