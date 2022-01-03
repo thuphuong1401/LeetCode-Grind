ba5886ab275930e8d6249be43d269004c7bde50e
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2022-01-02 19:02:20 -0500

    Update 2092.Find-All-People-With-Secret.java

--- Union-Find/2092.Find-All-People-With-Secret.java
+++ Union-Find/2092.Find-All-People-With-Secret.java
@@ -1,5 +1,7 @@
 /*
 https://leetcode.com/problems/find-all-people-with-secret/
+
+See solution 2 for a more "ingenious" version
 */
 
 class DSU {
@@ -122,3 +124,116 @@ class Solution {
     }
 }
 
+
+/*
+Big idea: "disconnect"
+
+- Sort the meetings from small to large by time 
+- For each timestamp, union all people with meetings in that timestamp (since 
+1 people know secret in a timestamp => all people from such component knows that 
+secret)
+- Important: after each timestamp union, disconnect all people not in the same
+component as 0. This means they don't have the secret by this timestamp, even
+though they might be connected to another person(s)
+- Repeat step 2 and 3 for all meetings. Add all person(s) in the same component
+as 0 to the final answer.
+*/
+
+class DSU {
+    int[] parents;
+    int[] size;
+    int N;
+
+    public DSU (int n) {
+        N = n;
+        parents = new int[n];
+        size = new int[n];
+        for(int i = 0; i < n; i++) {
+            parents[i] = i;
+            size[i] = 1;
+        }
+    }
+    
+    public int find (int p) {
+        int u = p;
+        while(u != parents[u]) {
+            u = parents[u];
+        }
+        while(p != u) {
+            int parentP = parents[p];
+            parents[p] = u;
+            p = parentP;
+        }
+        return u;
+    }
+    
+    public void union (int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if(rootP == rootQ) {
+            return;
+        } 
+        
+        if(size[rootP] > size[rootQ]) {
+            parents[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        } else {
+            parents[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        }
+    }
+    
+    
+    public boolean sameComponent (int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        return rootP == rootQ;
+    }
+    
+    public void reset (int p) {
+        parents[p] = p;
+    }
+}
+
+class Solution {
+    public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
+        Arrays.sort(meetings, (m1, m2) -> Integer.compare(m1[2], m2[2]));
+        DSU dsu = new DSU(n);
+        int m = meetings.length;
+        int i = 0;
+        dsu.union(0, firstPerson);
+        
+        List<Integer> secret = new ArrayList<>();
+        
+        while(i < m) {
+            List<Integer> people = new ArrayList<>(); // list of people who have meetings at this timestamp
+            int currTime = meetings[i][2];
+            while(i < m && meetings[i][2] == currTime) { // loop through meetings in the same timestamp
+                
+                int[] currMeeting = meetings[i];
+                int xi = currMeeting[0];
+                int yi = currMeeting[1];
+                dsu.union(xi, yi); // union these 2 people since they can share secrets with each other
+                people.add(xi);
+                people.add(yi);
+                
+                i++;
+                
+            }
+            
+            for(int p : people) {
+                if(!dsu.sameComponent(p, 0)) { // if a person having a meeting at this timestamp is not connected to 0, then he doesn't have the secret
+                    dsu.reset(p);
+                }
+            }
+        }
+        
+        for(int p = 0; p < n; p++) {
+            if(dsu.sameComponent(p, 0)) {
+                secret.add(p);
+            }
+        }
+        
+        return secret;
+    }
+}
