0c5dbed4266640c75bacdec07ff3c3badd9e2826
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2022-01-19 09:34:48 -0500

    Update 343.Integer-Break.java

--- Dynamic Programming/343.Integer-Break.java
+++ Dynamic Programming/343.Integer-Break.java
@@ -13,6 +13,12 @@ class Solution {
         for(int i = 3; i <= n; i++) {
             int maxProduct = -1;
             for(int j = 1; j < i; j++) {
+                /*
+                It could be that j > dp[j] therefore we need to get max
+                Ex:
+                dp[2] = 1 + 1 => 1 * 1 = 1 (constrant: need to separate into k >= 2 operands
+                However, dp[4] = 2 + 2 => 2 * 2 = 4
+                */
                 maxProduct = Math.max(maxProduct, Math.max(j, dp[j]) * Math.max(i - j, dp[i-j]));
             }
             dp[i] = maxProduct;
